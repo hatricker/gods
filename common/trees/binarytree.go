c5c30f3011b51ae32ef04c6e62fd5fadb0e27622
Changzheng Liu <cliu23@gmail.com>
2019-09-14 23:46:46 -0700

    Add binary tree marshal and unmarshal

---
+++ common/trees/binarytree.go
@@ -0,0 +1,88 @@
+package trees
+
+//TreeNode defines a binary tree node
+type TreeNode struct {
+	Val         int32
+	Left, Right *TreeNode
+}
+
+const (
+	//MinInt32 is the minimum value of type int32
+	MinInt32 = -1 << 31
+)
+
+//Unmarshal builds a binary tree from a series of integers
+func Unmarshal(values []int32) *TreeNode {
+	length := len(values)
+	if length == 0 || values[0] == MinInt32 {
+		return nil
+	}
+	queue := make([]*TreeNode, 0, length)
+	root := &TreeNode{Val: values[0]}
+	queue = append(queue, root)
+	for index := 1; index < length; index++ {
+		node := queue[0]
+		if values[index] != MinInt32 {
+			node.Left = &TreeNode{Val: values[index]}
+			queue = append(queue, node.Left)
+		}
+		index++
+		if index == length {
+			break
+		}
+
+		if values[index] != MinInt32 {
+			node.Right = &TreeNode{Val: values[index]}
+			queue = append(queue, node.Right)
+		}
+		queue = queue[1:]
+	}
+	return root
+}
+
+//appendNodeVal appends node value to serialized array including nils ahead of current node
+func appendNodeVal(a []int32, nodeVal int32, numNils int) []int32 {
+	if numNils > 0 {
+		tail := make([]int32, numNils)
+		for i := range tail {
+			tail[i] = MinInt32
+		}
+		a = append(a, tail...)
+	}
+	a = append(a, nodeVal)
+	return a
+}
+
+//Marshal serializes binary tree in BFS order
+func Marshal(root *TreeNode) []int32 {
+	if root == nil || root.Val == MinInt32 {
+		return nil
+	}
+	queue := make([]*TreeNode, 0, 64)
+	result := make([]int32, 0, 64)
+
+	queue = append(queue, root)
+	result = append(result, root.Val)
+	numNils := 0
+
+	for len(queue) > 0 {
+		top := queue[0]
+		if top.Left != nil {
+			queue = append(queue, top.Left)
+			result = appendNodeVal(result, top.Left.Val, numNils)
+			numNils = 0
+		} else {
+			numNils++
+		}
+
+		if top.Right != nil {
+			queue = append(queue, top.Right)
+			result = appendNodeVal(result, top.Right.Val, numNils)
+			numNils = 0
+		} else {
+			numNils++
+		}
+		queue = queue[1:]
+	}
+	return result
+}
---
+++ common/trees/binarytree_test.go
@@ -0,0 +1,66 @@
+package trees
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestMarshal(t *testing.T) {
+	assert := assert.New(t)
+
+	root := &TreeNode{Val: 1}
+	root.Left = &TreeNode{Val: 3}
+	root.Right = &TreeNode{Val: 2}
+	assert.Equal([]int32{1, 3, 2}, Marshal(root))
+
+	root = &TreeNode{Val: 1}
+	root.Right = &TreeNode{Val: 3}
+	root.Right.Left = &TreeNode{Val: 2}
+	assert.Equal([]int32{1, MinInt32, 3, 2}, Marshal(root))
+
+	root = &TreeNode{Val: 1}
+	root.Left = &TreeNode{Val: 3}
+	root.Left.Right = &TreeNode{Val: 2}
+	assert.Equal([]int32{1, 3, MinInt32, MinInt32, 2}, Marshal(root))
+
+	root = &TreeNode{Val: 1}
+	root.Left = &TreeNode{Val: 3}
+	root.Right = &TreeNode{Val: 2}
+	root.Left.Left = &TreeNode{Val: 4}
+	root.Right.Right = &TreeNode{Val: 5}
+	assert.Equal([]int32{1, 3, 2, 4, MinInt32, MinInt32, 5}, Marshal(root))
+
+	root = &TreeNode{Val: 1}
+	root.Left = &TreeNode{Val: 3}
+	root.Right = &TreeNode{Val: 2}
+	root.Left.Right = &TreeNode{Val: 4}
+	root.Right.Right = &TreeNode{Val: 5}
+	assert.Equal([]int32{1, 3, 2, MinInt32, 4, MinInt32, 5}, Marshal(root))
+}
+
+func TestUnmarshal(t *testing.T) {
+	assert := assert.New(t)
+
+	tests := []struct {
+		input, result []int32
+	}{
+		{
+			[]int32{1, 3, 2, 4, MinInt32, MinInt32, 5},
+			[]int32{1, 3, 2, 4, MinInt32, MinInt32, 5},
+		},
+		{
+			[]int32{1, 3, MinInt32, MinInt32, 2},
+			[]int32{1, 3, MinInt32, MinInt32, 2},
+		},
+		{
+			[]int32{1, 3, 2, MinInt32, MinInt32},
+			[]int32{1, 3, 2},
+		},
+	}
+
+	for _, tt := range tests {
+		result := Marshal(Unmarshal(tt.input))
+		assert.Equal(tt.result, result)
+	}
+}
